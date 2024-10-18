e51aacf5b69901a480fb4c24906c177f813ee752
Timothy Concepcion <timothyjtc@outlook.com>
2024-10-18 15:35:20 +0800

    adjust thresh params

--- ocr.py
+++ ocr.py
@@ -9,7 +9,7 @@ def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  
 def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    return cv2.threshold(image, 0, 128, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 def opening(image):
     kernel = np.ones((5, 5), np.uint8)
@@ -23,11 +23,11 @@ def denoise(image):
 
 gray = get_grayscale(img_source)
 denoised = denoise(gray)
-thresh = thresholding(gray)
+thresh = thresholding(denoised)
 opening = opening(gray)
 canny = canny(gray)
  
-for img in [img_source, denoised, thresh, opening, canny]:
+for img in [img_source, gray, thresh, opening, canny]:
     d = pytesseract.image_to_data(img, output_type=Output.DICT)
     n_boxes = len(d['text'])
  
@@ -36,12 +36,12 @@ for img in [img_source, denoised, thresh, opening, canny]:
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
  
     for i in range(n_boxes):
-        if int(d['conf'][i]) > 60:
+        if int(d['conf'][i]) > 40:
             (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
             # don't show empty text
             if text and text.strip() != "":
                 img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                img = cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)
+                img = cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 3)
  
     cv2.imshow('img', img)
     cv2.waitKey(0)
